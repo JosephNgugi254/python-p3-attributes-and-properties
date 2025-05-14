21ced87f7901c82bf9a1673a2e5ca8f0d4a25d56
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-05-14 12:19:58 +0300

    added attributes and properties

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,34 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John", job="Admin"):
+        self.name = name
+        self.job = job
+
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title() 
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self.set_name(name)
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        self.set_job(job)
